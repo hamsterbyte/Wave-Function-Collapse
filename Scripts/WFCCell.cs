7e9d6bb8ac050ed4bf8874a9ffd6aab02b75b074
hamsterbyte <hamsterbyte@hamsterbyte.com>
2023-10-16 22:24:57 -0700

    end commit

--- Scripts/WFCCell.cs
+++ Scripts/WFCCell.cs
@@ -4,24 +4,49 @@
 
     public partial class WFCCell{
         private void PrecalculateFrequencies(){
-            
+            for (int i = 0; i < rawFrequencies.Length; i++){
+                logFrequencies[i] = Math.Log2(rawFrequencies[i]);
+            }
+
+            sumOfRawFrequencies = rawFrequencies.Sum();
+            sumOfPossibleFrequencies = sumOfRawFrequencies;
+            for (int i = 0; i < rawFrequencies.Length; i++){
+                sumOfPossibleFrequencyLogFrequencies += Math.Log2(sumOfRawFrequencies) * Math.Log2(rawFrequencies[i]);
+            }
         }
 
         public void RemoveOption(int i){
-            
+            Options[i] = false;
+            sumOfPossibleFrequencies -= rawFrequencies[i];
+            sumOfPossibleFrequencyLogFrequencies -= logFrequencies[i];
         }
 
-        public double Entropy => 0;
+        public double Entropy => Math.Log2(sumOfPossibleFrequencies) - sumOfPossibleFrequencyLogFrequencies/sumOfPossibleFrequencies + entropyNoise;
 
 
         private int WeightedRandomIndex(){
-            
+            int pointer = 0;
+            int randomFromSumPossible = WFCGrid.Random.Next(0, sumOfPossibleFrequencies);
+            for (int i = 0; i < Options.Length; i++){
+                if (!Options[i]) continue;
+                pointer += rawFrequencies[i];
+                if (pointer >= randomFromSumPossible){
+                    return i;
+                }
+            }
+            //If index returns -1 we know the collapse has failed.
             return -1;
         }
 
 
         public int Collapse(){
-            return -1;
+            int weightedRandomIndex = WeightedRandomIndex();
+            TileIndex = weightedRandomIndex;
+            Collapsed = true;
+            for (int i = 0; i < Options.Length; i++){
+                Options[i] = i == TileIndex;
+            }
+            return weightedRandomIndex;
         }
     }
 }
--- Scripts/WFCGrid.cs
+++ Scripts/WFCGrid.cs
@@ -3,19 +3,113 @@
 
     public partial class WFCGrid{
         private EntropyCoordinates Observe(){
+            while (!entropyHeap.IsEmpty){
+                EntropyCoordinates coords = entropyHeap.Pop();
+                if (!cells[coords.Coordinates.X, coords.Coordinates.Y].Collapsed) return coords;
+            }
             return EntropyCoordinates.Invalid;
         }
         
         private void Collapse(Coordinates _coords){
-            
+            int collapsedIndex = cells[_coords.X, _coords.Y].Collapse();
+            AnimationCoordinates.Enqueue(_coords);
+            removalUpdates.Push(new RemovalUpdate(){
+                Coordinates = _coords,
+                TileIndex = collapsedIndex
+            });
+            remainingUncollapsedCells--;
         }
-        
-        private void Propagate(bool _wrap = true){
 
+        private void Propagate(bool _wrap = true){
+            while (removalUpdates.Count > 0){
+                RemovalUpdate update = removalUpdates.Pop();
+                if (update.TileIndex == -1){
+                    validCollapse = false;
+                    return;
+                }
+
+                Coordinates[] cardinals = Coordinates.Cardinals;
+                for (int d = 0; d < adjacencyRules.GetLength(1); d++){
+                    Coordinates current = cardinals[d] + update.Coordinates;
+                    if (_wrap){
+                        current = current.Wrap(Width, Height);
+                    } else if (!IsInBounds(current)){
+                        continue;
+                    }
+
+                    WFCCell currentCell = cells[current.X, current.Y];
+                    if (currentCell.Collapsed) continue;
+                    for (int o = 0; o < adjacencyRules.GetLength(2); o++){
+                        if (adjacencyRules[update.TileIndex, d, o] == 0 && currentCell.Options[o]){
+                            currentCell.RemoveOption(o);
+                        }
+                    }
+                    entropyHeap.Push(new EntropyCoordinates(){
+                        Coordinates = currentCell.Coordinates,
+                        Entropy = currentCell.Entropy
+                    });
+                }
+            }
         }
         
         public void TryCollapse(bool _wrap = true, int _maxAttempts = 100){
+            Reset(true);
+            Busy = true;
+            Stopwatch timer = Stopwatch.StartNew();
+            for (int i = 0; i < _maxAttempts; i++){
+                currentAttempt++;
+                WFCCell cell = cells.Random();
+                entropyHeap.Push(new EntropyCoordinates(){
+                    Coordinates = cell.Coordinates,
+                    Entropy = cell.Entropy
+                });
 
+                while (remainingUncollapsedCells > 0){
+                    EntropyCoordinates e = Observe();
+                    Collapse(e.Coordinates);
+                    Propagate(_wrap);
+                }
+
+                if (!validCollapse && i < _maxAttempts - 1){
+                    Reset();
+                }
+                else{
+                    break;
+                }
+            }
+            timer.Stop();
+            WFCResult result = new(){
+                Grid = this,
+                Success = validCollapse,
+                Attempts = currentAttempt,
+                ElapsedMilliseconds = timer.ElapsedMilliseconds
+            };
+            onComplete?.Invoke(result);
+            Busy = false;
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
